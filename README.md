879fb1e007e845b4c1b0ba7b74a44dc60aeb1051
liuwei <liuwei29507@goldwind.com.cn>
2017-11-27 14:56:55 +0800

    添加README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# 实践webpack的指南
+
+> 官网地址 -[https://doc.webpack-china.org/guides]
+
+- 起步
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     entry:'./src/index.js',
     output:{
-        filename:'bundle.[hash:8].js',
+        filename:'bundle.js',
         path:path.resolve(__dirname,'dist')
     }
 };
